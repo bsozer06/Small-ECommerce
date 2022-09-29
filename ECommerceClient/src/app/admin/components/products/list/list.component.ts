0f2009b8307f675d65f3b31d8fcd16ec35443eb1
burhan sozer <you@example.com>
2022-09-30 00:12:27 +0300

    Developed the directive for deleting

--- ECommerceClient/src/app/admin/components/products/list/list.component.ts
+++ ECommerceClient/src/app/admin/components/products/list/list.component.ts
@@ -9,6 +9,8 @@ import { Product_Pagination } from 'src/app/contracts/product_pagination';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -23,7 +25,7 @@ export class ListComponent extends BaseComponent implements OnInit {
     super(spinner);
   }
 
-  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate'];
+  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate', 'edit', 'delete',];
   dataSource: MatTableDataSource<List_Product> = null;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -41,6 +43,11 @@ export class ListComponent extends BaseComponent implements OnInit {
     this.dataSource = new MatTableDataSource<List_Product>(allProducts.products);
     this.paginator.length = allProducts.totalCount;
   }
+
+  // delete(id, event) {
+  //   const img: HTMLImageElement = event.srcElement;
+  //   $(img.parentElement.parentElement).fadeOut(2000);
+  // }
 
   // pagination event'i
   async pageChange() {
