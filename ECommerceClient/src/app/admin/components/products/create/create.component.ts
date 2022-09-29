7d8067d72866da97907e66e9d92ec36ee7586e83
burhan sozer <you@example.com>
2022-09-29 13:32:30 +0300

    Added the inserting of products components

---
+++ ECommerceClient/src/app/admin/components/products/create/create.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Create_Product } from 'src/app/contracts/create_product';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { ProductService } from 'src/app/services/common/models/product.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss']
+})
+export class CreateComponent extends BaseComponent implements OnInit {
+
+  constructor(
+    private productService: ProductService,
+    private alertifyService: AlertifyService,
+    spinner: NgxSpinnerService
+    ) {
+      super(spinner);
+    }
+
+  ngOnInit(): void {
+  }
+
+  create(name:HTMLInputElement, stock:HTMLInputElement, price:HTMLInputElement) {
+    this.showSpinner(SpinnerType.BallAtom);
+    const create_product: Create_Product = new Create_Product();
+    create_product.name = name.value;
+    create_product.stock = parseInt(stock.value);
+    create_product.price = parseFloat(price.value);
+
+    this.productService.create(create_product , () => {
+      this.hideSpinner(SpinnerType.BallAtom);
+      this.alertifyService.message("Product was added successfully.", {
+        dismissOthers: true,
+        messageType: MessageType.Success,
+        position: Position.TopRight
+      });
+    });
+  }
+
+}
