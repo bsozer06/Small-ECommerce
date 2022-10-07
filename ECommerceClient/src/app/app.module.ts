5efc21e9c630a0d14715d2639422943ec9fb409c
burhan sozer <you@example.com>
2022-10-08 00:00:44 +0300

    Added the guard and auth0/angular-jwt

--- ECommerceClient/src/app/app.module.ts
+++ ECommerceClient/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
 
 
 
@@ -24,7 +25,13 @@ import { HttpClientModule } from '@angular/common/http';
     AdminModule, UiModule,
     ToastrModule.forRoot(),
     NgxSpinnerModule,
-    HttpClientModule
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => localStorage.getItem("accessToken"),
+        allowedDomains: ["localhost:7008"]
+      }
+    })
   ],
   providers: [
     { provide: "baseUrl", useValue: "https://localhost:7008/api", multi: true}
