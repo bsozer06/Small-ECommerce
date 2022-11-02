382b08daf6b00ab653b77ef11b158f0072046f97
burhan sozer <you@example.com>
2022-11-02 23:14:41 +0300

    Developed the role management on clientside

---
+++ ECommerceClient/src/app/dialogs/authorize-menu-dialog/authorize-menu-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BaseDialog } from '../base/base-dialog';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-authorize-menu-dialog',
+  templateUrl: './authorize-menu-dialog.component.html',
+  styleUrls: ['./authorize-menu-dialog.component.scss']
+})
+export class AuthorizeMenuDialogComponent extends BaseDialog<AuthorizeMenuDialogComponent> {
+
+  constructor(
+    dialogRef: MatDialogRef<AuthorizeMenuDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: AuthorizeMenuState | any
+  ) {
+    super(dialogRef);
+  }
+
+}
+
+export enum AuthorizeMenuState {
+  Yes,
+  No
+}
--- ECommerceClient/src/app/dialogs/dialogs.module.ts
+++ ECommerceClient/src/app/dialogs/dialogs.module.ts
@@ -14,6 +14,8 @@ import { OrderDetailDialogComponent } from './order-detail-dialog/order-detail-d
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CompleteOrderDialogComponent } from './complete-order-dialog/complete-order-dialog.component';
+import { AuthorizeMenuDialogComponent } from './authorize-menu-dialog/authorize-menu-dialog.component';
+import { MatBadgeModule } from '@angular/material/badge';
 
 
 
@@ -25,13 +27,15 @@ import { CompleteOrderDialogComponent } from './complete-order-dialog/complete-o
     BasketItemRemoveDialogComponent,
     ShoppingCompleteDialogComponent,
     OrderDetailDialogComponent,
-    CompleteOrderDialogComponent
+    CompleteOrderDialogComponent,
+    AuthorizeMenuDialogComponent
   ],
   imports: [
     CommonModule,
     MatDialogModule, MatButtonModule, MatCardModule, MatTableModule, MatToolbarModule,
     FileUploadModule,
-    FormsModule
+    FormsModule,
+    MatBadgeModule
   ]
 })
 export class DialogsModule { }
